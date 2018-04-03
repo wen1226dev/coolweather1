13f0127c26106bad9f9d4227cfe074f9dabce992
Wen <1205948504@qq.com>
2018-04-03 14:24:14 +0800

    加入显示天气信息的功能。

---
+++ app/src/main/java/com/coolweather1/android/gson/AQI.java
@@ -0,0 +1,15 @@
+package com.coolweather1.android.gson;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
---
+++ app/src/main/java/com/coolweather1/android/gson/Now.java
@@ -0,0 +1,26 @@
+package com.coolweather1.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Administrator on 2018/4/2.
+ */
+
+
+
+public class Now {
+
+    @SerializedName("tmp")
+    public String temperature;
+
+    @SerializedName("cond")
+    public More more;
+
+    public class More {
+
+        @SerializedName("txt")
+        public String info;
+
+    }
+
+}
